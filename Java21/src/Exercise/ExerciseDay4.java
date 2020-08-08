b35c769995486ae8e0e88e8358359545f940f197
demnofocus <47452568+demnofocus@users.noreply.github.com>
2020-08-09 00:20:37 +0530

    created: ArrayCopier.java, ExerciseDay4, HalfDollar.java
    commit contains new work of day 4

---
+++ Java21/src/Exercise/ExerciseDay4.java
@@ -0,0 +1,82 @@
+package Exercise;
+
+class ExerciseDay4{
+    /*
+        Certification Practice
+    */
+    static void cases(){
+        float x = 10, y = 5;
+        int z = (int)(x / y);
+        System.out.println("Value of z: " + z);
+
+        switch(z){
+            case 1:
+                x = x + 2;
+                System.out.println("Value of x: " + x);
+            case 2:
+                x = x + 3;
+                System.out.println("Value of x: " + x);
+            default:
+                x = x + 1;    
+                System.out.println("Value of x: " + x);
+            }
+        System.out.println("Value of x: " + x);
+    }
+    
+    //01
+    static int countDays(int month, int year){
+        int count = -1;
+        switch(month){
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                count = 31;
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                count = 30;
+                break;
+            case 2:
+                if(year % 4 == 0){
+                    count = 29;
+                }else{
+                    count = 28;
+                }
+                if((year % 100 == 0) & (year % 400 != 0)){
+                    count = 28;
+                }
+        }
+        return count;
+    }
+    
+    public static void main(String[] arguments){
+       /*
+         Practice
+       */
+       ExerciseDay4.cases();
+       
+       /*
+         Exercise 01
+       */
+       
+       //year = 2020
+       String[] monthList = new String[]{"January", "February", "March", "April",
+           "May", "June", "July", "August", "September", "October", "November", "December"};
+       System.out.println("Dates of 2020: ");
+       for(int i = 1; i<=12; i++){
+           int totalDays = ExerciseDay4.countDays(i,2020);
+           for(int j=1; j<=totalDays; j++){
+               System.out.println("Date: " + monthList[i-1] + " " + j);
+           }
+           System.out.println("*");
+       }
+       
+    }
+}
+
