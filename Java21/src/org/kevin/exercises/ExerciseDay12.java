12e0d8ec04f5239c8acf7968dcb8a3363abbbdc6
demnofocus <47452568+demnofocus@users.noreply.github.com>
2021-05-29 18:08:55 +0530

    Create ExerciseDay12.java
    Grid layout exercise

---
+++ Java21/src/org/kevin/exercises/ExerciseDay12.java
@@ -0,0 +1,35 @@
+package org.kevin.exercises;
+
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ * Grid Layouts
+ */
+public class ExerciseDay12 extends JFrame {
+    public ExerciseDay12() {
+        //Three Buttons
+        super("Program");
+        super.setSize(350, 225);
+        super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JButton alpha = new JButton("Alpha");
+        JButton beta = new JButton("Beta");
+        JButton gamma = new JButton("Gamma");
+        JPanel content = new JPanel();
+        //answer
+        content.setLayout(new FlowLayout());
+        content.add(alpha);
+        content.add(beta);
+        content.add(gamma);
+        super.add(content);
+        super.pack();
+        super.setVisible(true); 
+    }
+    
+    public static void main(String[] arguments){
+        ExerciseDay12 b3 = new ExerciseDay12();
+    }
+}
